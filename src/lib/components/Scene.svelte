4ccb0d4427ad03a61829c4fe4f565672b7d14730
mili <kamil.maksudov2019@gmail.com>
2023-05-20 13:17:50 +0200

    feat: first scene

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { T, useFrame } from '@threlte/core';
+	import { interactivity } from '@threlte/extras';
+	import { spring } from 'svelte/motion';
+
+	interactivity();
+	const scale = spring(1);
+
+	let rotation = 0;
+	useFrame((state, delta) => (rotation += delta));
+</script>
+
+<!-- Camera -->
+<T.PerspectiveCamera
+	makeDefault
+	position={[10, 10, 10]}
+	on:create={({ ref }) => {
+		ref.lookAt(0, 0, 0);
+	}}
+/>
+
+<!-- Lights -->
+<T.DirectionalLight position={[3, 10, 7]} intensity={1.5} />
+
+<T.Mesh
+	rotation.y={rotation}
+	position.y={1}
+	scale={$scale}
+	on:pointerenter={() => scale.set(1.5)}
+	on:pointerleave={() => scale.set(1)}
+>
+	<T.BoxGeometry args={[1, 5, 2]} />
+	<T.MeshStandardMaterial color="green" />
+</T.Mesh>
