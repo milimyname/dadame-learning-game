360d79558f3fe1b8437dfa692f3f63a2758f70ab
mili <kamil.maksudov2019@gmail.com>
2023-05-20 13:23:29 +0200

    feat: add vercel adapter

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import { T, useFrame } from '@threlte/core';
-	import { interactivity } from '@threlte/extras';
+	import { interactivity, OrbitControls } from '@threlte/extras';
 	import { spring } from 'svelte/motion';
 
 	interactivity();
 	const scale = spring(1);
-
-	let rotation = 0;
-	useFrame((state, delta) => (rotation += delta));
+	// let rotation = 0;
+	// useFrame((state, delta) => (rotation += delta));
 </script>
 
 <!-- Camera -->
@@ -17,18 +16,14 @@
 	on:create={({ ref }) => {
 		ref.lookAt(0, 0, 0);
 	}}
-/>
+>
+	<OrbitControls enableDamping autoRotate />
+</T.PerspectiveCamera>
 
 <!-- Lights -->
 <T.DirectionalLight position={[3, 10, 7]} intensity={1.5} />
 
-<T.Mesh
-	rotation.y={rotation}
-	position.y={1}
-	scale={$scale}
-	on:pointerenter={() => scale.set(1.5)}
-	on:pointerleave={() => scale.set(1)}
->
+<T.Mesh scale={$scale} on:pointerenter={() => scale.set(1.5)} on:pointerleave={() => scale.set(1)}>
 	<T.BoxGeometry args={[1, 5, 2]} />
 	<T.MeshStandardMaterial color="green" />
 </T.Mesh>
